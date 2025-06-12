1ce637bda341faabeca84fcab4c439cefe3bb9e3
Davi Ribeiro <davi.75.ef@gmail.com>
2025-06-12 17:55:40 -0300

    feat: add real number

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 export const address = "Maria Gomes de Sá, 161b\nMondubim, Fortaleza-Ce";
-export const telephone = "(85) 1234-1234";
-export const wpp = "(85) 1234-1234";
+export const telephone = "(85) 99680-3928";
+export const wpp = "(85) 989296545";
 export const email = "contato@neresfarma.com.br";
