547eb9f7373ac211ba1a3e0be67551416363c752
Davi Ribeiro <davi.75.ef@gmail.com>
2025-06-12 18:59:13 -0300

    fix: remove unused codes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Contact from "./components/Contact";
 import Logo from "../../assets/logo.svg";
 
